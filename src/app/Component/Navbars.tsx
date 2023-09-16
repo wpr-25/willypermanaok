63ac3725491bbcd5c977658ec06b0b3ddbd95b92
wpr-25 <willy.ok1025@gmail.com>
2023-09-16 14:08:57 +0700

    first

--- src/app/Component/Navbars.tsx
+++ src/app/Component/Navbars.tsx
@@ -70,7 +70,6 @@ export default function Navbars() {
             <Navbar className="sticky inset-0 z-10 h-max max-w-full rounded-none py-2 px-4 lg:px-8 lg:py-4 bg-gradient-to-tl from-purple-200 to via-white bg-cyan-300">
                 <div className="flex items-center justify-between text-blue-gray-900">
                     <Link
-                        as="a"
                         href="/"
                         className="mr-4 cursor-pointer py-1.5 font-medium"
                     >
