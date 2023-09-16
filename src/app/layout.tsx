9fdb34411f44b9685f464b60e182f90cdd4dc3a9
wpr-25 <willy.ok1025@gmail.com>
2023-09-16 13:02:10 +0700

    first

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbars from './Component/Navbars'
 import Footer from './Component/Footer'
+import { Analytics } from '@vercel/analytics/react'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbars />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
