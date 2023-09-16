6450c5d37fa5ca0f5f016c5bdc4187c396bdcc25
wpr-25 <willy.ok1025@gmail.com>
2023-09-16 15:59:12 +0700

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,11 +73,11 @@ export default function Home() {
                   Willy Permana Ok
                 </p>
                 <p className="mt-3 text-gray-600 text-justify">
-                  Saya Membuat Website ini sebagai CV dan Portofolio untuk bisa ditampilkan kepada Anda.
-                  <br />Saya Membangun Website ini dengan Framework Next.Js tentunya dibantu oleh TailwindCss.
+                  Website ini bertujuan untuk menampilkan CV dan Portofolio
+                  <br />
                 </p>
                 <Link href="/Pengalaman-Perkerjaan" className="inline-flex gap-x-1 items-center text-indigo-600 hover:text-indigo-500 duration-150 font-medium">
-                  Cari tahu Pengalaman Saya
+                  Profile
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                     <path fillRule="evenodd" d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z" clipRule="evenodd" />
                   </svg>
@@ -181,7 +181,7 @@ export default function Home() {
                 title="Visit the East"
                 className="inline-block mb-3 text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700 text-black"
               >
-                Jurnal yang Saya buat
+                Karya Tulis yang dibuat
               </Link>
               <p className="mb-2 text-gray-700">
                 Jurnal Analisis Behavior Intention ......
@@ -202,7 +202,7 @@ export default function Home() {
                   aria-label="Category"
                   title="traveling"
                 >
-                  Organisasi yang Saya ikuti
+                  Organisasi Aktiv
                 </a>
 
               </p>
@@ -233,7 +233,7 @@ export default function Home() {
                   aria-label="Category"
                   title="traveling"
                 >
-                  Pembicara IT Security
+                  Narasumber IT Security
                 </Link>
               </p>
               <a
@@ -242,7 +242,7 @@ export default function Home() {
                 title="Film It!"
                 className="inline-block mb-3 text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700 text-black"
               >
-                Pembicara IT Security
+                Narasumber Security
               </a>
               <p className="mb-2 text-gray-700">
                 Diundang kreator programming.
