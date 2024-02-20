8ca132733377fd6fd4b89c2526dc5d313923fc14
wpr-25 <110904048+wpr-25@users.noreply.github.com>
2024-02-20 17:27:27 +0700

    Update GoogleAnalytics.jsx

--- src/app/GoogleAnalytics.jsx
+++ src/app/GoogleAnalytics.jsx
@@ -12,7 +12,7 @@ const GoogleAnalytics = () => {
         <>
             <Script
                 strategy="afterInteractive"
-                src={`https://www.googletagmanager.com/gtag/js?id=${gtag.G-SWLRRNE0EV}`}
+                src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
             />
             <Script
                 id="gtag-init"
@@ -22,7 +22,7 @@ const GoogleAnalytics = () => {
                       window.dataLayer = window.dataLayer || [];
                       function gtag(){dataLayer.push(arguments);}
                       gtag('js', new Date());
-                      gtag('config', '${gtag.G-SWLRRNE0EV}', {
+                      gtag('config', '${gtag.GA_TRACKING_ID}', {
                       page_path: window.location.pathname,
                       });
                     `,
